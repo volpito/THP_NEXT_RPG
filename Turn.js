5c911f7aa15093d03f8311b66d0d7cd2d308b9ca
sassok <kevin.sassoo@gmail.com>
2021-04-07 20:54:57 +0200

    new character

--- Turn.js
+++ Turn.js
@@ -9,22 +9,34 @@ class Turn extends Game {
   fight = (firstPlayer, secondPlayer, thirdPlayer) => {
     for(this.turnCount = 1; this.turnCount <= 10;this.turnCount++){
       let perso = fighters[Math.floor(Math.random() * fighters.length)];
-      const pick = fighters.filter(c => c.name != perso.name);
-      const name = pick.map(a => a.name);
-      console.log(name);
-      console.log(`Pour ce tour vous allez incarner ${perso.name}`);
 
-      let foo = prompt(`Quel attaquand choisissez-vous d attaquer ? ${name[0]} ou ${name[1]} ?`);
+      const pick = fighters.filter(c => c.name != perso.name);
+      
+      const name = pick.map(a => a.name);
+      
+      console.log("")
+      console.log(`Round numéro ${this.turnCount}`)
+      console.log("")
+      console.log(`Pour ce tour vous allez incarner ${perso.name}`);
+      console.log(`Choisi ta prochaine victime: ${name[0]} ou ${name[1]} ?`);
+      console.log("")
+
+      let foo = prompt(`Quel combattant choisissez-vous d attaquer ? ${name[0]} ou ${name[1]} ?`);
+      console.log(`Attaque éclaire de ${perso.name} de ${perso.dmg} sur la victime ${foo}`)
       if (game1.firstPlayer.name == foo){
           firstPlayer.hp = firstPlayer.hp - perso.dmg;
-          console.log("Michel a désormais " + firstPlayer.hp + "HP ! BIM")
+          console.log("Ulder a désormais " + firstPlayer.hp + "HP ! BIM")
       }
 
       if (game1.secondPlayer.name == foo){
         secondPlayer.hp = secondPlayer.hp - perso.dmg;
-        console.log("Micheline a désormais " + secondPlayer.hp + "HP ! BIM")
+        console.log("Grace a désormais " + secondPlayer.hp + "HP ! BIM")
+      }
 
-    }
+      if (game1.thirdPlayer.name == foo){
+        thirdPlayer.hp = thirdPlayer.hp - perso.dmg;
+        console.log("Moana a désormais " + thirdPlayer.hp + "HP ! BIM")
+      }
 
       if (firstPlayer.hp <= 0){
         console.log("Player 1 Dead")
@@ -38,7 +50,7 @@ class Turn extends Game {
         console.log("PLayer 3 Dead")
         break;
       }
-    console.log(`Round numéro ${this.turnCount}`)
+    
     }
 }
 }
