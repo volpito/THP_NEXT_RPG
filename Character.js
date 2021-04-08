b902358f1fad9dff1035001fc34b012bd3385b69
volpito <mpj.rigot@gmail.com>
2021-04-08 17:27:44 +0200

    emoji

--- Character.js
+++ Character.js
@@ -19,7 +19,7 @@ class Character {
   dealDamage = (victim) => {
     if (this.status != "loser"){
       victim.takeDamage(this.dmg);
-      console.log(`Attaque éclair de ${this.name} de ${this.dmg} sur la victime ${victim.name} 🌟 🥊 🚑`)
+      console.log(`Attaque éclair de ${this.name} de ${this.dmg} sur la victime ${victim.name} 💥🥊 🚑`)
       console.log(victim.name + " a désormais " + victim.hp + " HP ! BIM")
     }
   }
